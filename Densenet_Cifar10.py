2c197d095b599a2e93993dded90e083181f3282c
Xolisani Nkwentsha <Xolisani Nkwentsha>
2019-06-03 20:44:20 +0200

    ghh

--- Densenet_Cifar10.py
+++ Densenet_Cifar10.py
@@ -396,7 +396,7 @@ with tf.Session() as sess:
             cost,_,_  = sess.run([loss, opt, iou_update], feed_dict=train_feed_dict)
             train_iou = sess.run(iou, feed_dict=train_feed_dict)
             #print(sess.run([loss, opt, iou_update], feed_dict=train_feed_dict))
-            print(cost)
+            print(loss)
             #loss_ = loss.eval()
 
             train_loss += cost
