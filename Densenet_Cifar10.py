c1884341a40adc1b428202c9c1eb49f8536ea9a8
Xolisani Nkwentsha <Xolisani Nkwentsha>
2019-06-04 10:56:59 +0200

    dasda

--- Densenet_Cifar10.py
+++ Densenet_Cifar10.py
@@ -131,8 +131,10 @@ def xentropy_loss(logits, labels, num_classes):
         loss: The cross entropy loss over each image in the batch.
     """
     labels = tf.cast(labels, tf.float32)
+
     logits = tf.reshape(logits, [logits.get_shape()[1],logits.get_shape()[2],3, num_classes])
     labels = tf.reshape(labels, [labels.get_shape()[1],labels.get_shape()[2], labels.get_shape()[3]])
+    labels = tf.reshape(tf.one_hot(tf.squeeze(labels), depth = num_classes)
     #loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
     #        logits=logits, labels=labels, name="loss")
 
