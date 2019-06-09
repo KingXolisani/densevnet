5d37e891f1264f2aec2b93da5937ccb0e7837fd6
Xolisani Nkwentsha <Xolisani Nkwentsha>
2019-06-09 16:35:05 +0200

    ''

--- Densenet_Cifar10.py
+++ Densenet_Cifar10.py
@@ -193,7 +193,7 @@ def calculate_iou(mask, prediction, num_classes):
     mask = tf.reshape(tf.one_hot(tf.squeeze(mask), depth = num_classes), [tf.shape(mask)[0], -1, num_classes])
     #prediction = tf.reshape(prediction, shape=[prediction.get_shape()[1],prediction.get_shape()[2], 1, num_classes])
     prediction = tf.reshape(prediction, shape=[tf.shape(prediction)[0], -1, num_classes])
-    iou, update_op = tf.metrics.mean_iou(tf.argmax(prediction, 1), tf.argmax(mask, 1), num_classes)
+    iou, update_op = tf.metrics.mean_iou(tf.argmax(prediction, 3), tf.argmax(mask, 3), num_classes)
 
     return iou, update_op
 
