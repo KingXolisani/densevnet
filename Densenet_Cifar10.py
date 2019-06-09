f48f4b0ec25113c9684c04eaf473965b016f83e6
Xolisani Nkwentsha <Xolisani Nkwentsha>
2019-06-09 14:15:05 +0200

    checking loss outputs

--- Densenet_Cifar10.py
+++ Densenet_Cifar10.py
@@ -381,11 +381,11 @@ with tf.Session() as sess:
                 training : True
             }
 
-            print(batch_x)
-            print(batch_y)
             cost,_,_  = sess.run([loss, opt, iou_update], feed_dict=train_feed_dict)
             train_iou = sess.run(iou, feed_dict=train_feed_dict)
 
+            print (sess.run([loss, opt, iou_update], feed_dict=train_feed_dict))
+            print(train_iou)
             train_loss += cost
             train_acc += train_iou
             pre_index += batch_size
