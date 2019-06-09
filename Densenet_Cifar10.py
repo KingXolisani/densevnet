1d9747a4078b802d9d016d3a314f4c7d25640ac9
Xolisani Nkwentsha <Xolisani Nkwentsha>
2019-06-09 16:42:19 +0200

    ''

--- Densenet_Cifar10.py
+++ Densenet_Cifar10.py
@@ -169,6 +169,7 @@ def xentropy_loss(logits, labels, num_classes):
     """
     #labels = tf.one_hot(tf.squeeze(labels), depth = num_classes)
     labels = tf.cast(labels, tf.int32)
+    labels = labels < 21
     #logits = tf.reshape(logits, [logits.get_shape()[1],logits.get_shape()[2],1, num_classes])
     logits = tf.reshape(logits, [tf.shape(logits)[0], -1, num_classes])
     #labels = tf.reshape(labels, [labels.get_shape()[1],labels.get_shape()[2], labels.get_shape()[3]])
