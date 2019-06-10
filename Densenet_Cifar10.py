ee36f57619d0523508b2c955ea40f94d7284f5dd
Xolisani Nkwentsha <Xolisani Nkwentsha>
2019-06-10 15:05:06 +0200

    ''

--- Densenet_Cifar10.py
+++ Densenet_Cifar10.py
@@ -374,13 +374,11 @@ with tf.Session() as sess:
             #image_batch, mask_batch, _ = sess.run([image_ph, mask_ph, reset_iou])
             image_batch = x_train[train_step :train_step + batch_size]
             mask_batch = y_train[train_step :train_step + batch_size]
-            print(mask_batch[0][0][71])
-            for i in range(len(mask_batch[0])):
-                #print(mask_batch[0])
-                if mask_batch[0][i][0] > 20:
-                    mask_batch[0][i][0] = 20
-
-            break
+            for y in range(len(mask_batch[0])):
+                for x in range(len(mask_batch[0][y]))
+                    #print(mask_batch[0])
+                    if mask_batch[0][y][x] > 20:
+                        mask_batch[0][y][x] = 20
             feed_dict = {image_ph: image_batch,
                         mask_ph: mask_batch,
                         training: True}
